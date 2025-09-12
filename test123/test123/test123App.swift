410e8aee739bfb5516b00e976e127607ade1b959
Daniel <76424137+danielmulangu@users.noreply.github.com>
2025-09-11 21:13:21 -0500

    commit

---
+++ test123/test123/test123App.swift
@@ -0,0 +1,17 @@
+//
+//  test123App.swift
+//  test123
+//
+//  Created by Daniel Mulangu on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct test123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
