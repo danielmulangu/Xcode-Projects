95c37e27d7fbd015faf9ee86b25b30f6dab39ad5
Daniel <76424137+danielmulangu@users.noreply.github.com>
2025-09-17 12:37:04 -0500

    Add files via upload

---
+++ test project lesson9/test_project_lesson9App.swift
@@ -0,0 +1,17 @@
+//
+//  test_project_lesson9App.swift
+//  test project lesson9
+//
+//  Created by Daniel Mulangu on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct test_project_lesson9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
